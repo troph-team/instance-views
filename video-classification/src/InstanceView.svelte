f596331d521d7dead60fe367b641093bc4137caa
zebwu <wzb3422@gmail.com>
2024-01-15 21:43:39 +0800

    feat: video-classification

---
+++ video-classification/src/InstanceView.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  export let entry;
+  export let options;
+  export let modelColumn;
+  export let labelColumn;
+  export let dataColumn;
+  export let idColumn;
+</script>
+
+<div class="box" style:background-color="white">
+  <img
+    src={entry[dataColumn]}
+    style:max-width="200px"
+    alt="Image thumbnail for instance {entry[idColumn]}"
+  />
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video controls width="250">
+    <source src={entry[dataColumn]} type="video/mp4" />
+    Download the
+    <a href={entry[dataColumn]}>MP4</a>
+    video.
+  </video>
+  <br />
+  <div class="text">
+    <span class="label">label: </span>
+    <span class="value">{entry[labelColumn]}</span>
+  </div>
+  {#if modelColumn && entry[modelColumn]}
+    <div class="text">
+      <span class="label">output: </span>
+      <span class="value">{entry[modelColumn]} </span>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .label {
+    margin-right: 5px;
+    font-size: 10px;
+    color: rgba(0, 0, 0, 0.5);
+    font-variant: small-caps;
+  }
+  .text {
+    display: flex;
+    flex-direction: row;
+  }
+  .value {
+    font-size: 10px;
+  }
+  .box {
+    width: min-content;
+    padding: 5px;
+    border: 0.5px solid rgb(224, 224, 224);
+  }
+</style>
